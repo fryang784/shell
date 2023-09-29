a49019fb8cd7f2600b824c62686fc99b938f4065
Richard's Rockfish <fyang52@jhu.edu>
2023-09-29 13:27:30 -0400

    use env USER

--- refresh_job_status.sh
+++ refresh_job_status.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 userid='fyang52'
+echo $USER
 nap=1
-n=`squeue -hu $userid | wc -l`
+n=`squeue -hu $USER | wc -l`
 while [ $n -gt 0 ]; do 
 	clear
-	squeue -u $userid
+	squeue -u $USER
 	echo $n jobs running... refresh in $nap s
 	sleep $nap
     	echo refreshing...
-	n=`squeue -hu $userid | wc -l`
+	n=`squeue -hu $USER | wc -l`
 done
 echo "job done!"
